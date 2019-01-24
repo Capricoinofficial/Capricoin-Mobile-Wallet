97edb79bf431ef708c315b5c4e8a4e67800fb537
Maxim Prishchepo <repo@sfxdx.ru>
2019-01-24 17:10:13 +0700

    backstep

--- src/js/trezor-url.js
+++ src/js/trezor-url.js
@@ -1,2 +1,2 @@
 window.TREZOR_CHROME_URL = './bower_components/trezor-connect/chrome/wrapper.html';
-window.__TREZOR_CONNECT_SRC = 'http://192.168.88.12:8088/';
+window.__TREZOR_CONNECT_SRC = 'http://localhost:8088/';
